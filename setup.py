dd0b365d96c4d6170153290ea4598250dc705ae8
Chris Markiewicz <effigies@gmail.com>
2023-10-11 10:41:39 -0400

    MNT: Build wheels with the Python limited API

--- setup.py
+++ setup.py
@@ -5,6 +5,19 @@ extensions and a legacy script.
 """
 from setuptools import setup, Extension
 from numpy import get_include
+from wheel.bdist_wheel import bdist_wheel
+
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        # Declare support back to cp32
+        # Do not modify Python 2
+        if python.startswith('cp3'):
+            python, abi = 'cp32', 'abi3'
+
+        return python, abi, plat
 
 
 setup(
@@ -14,8 +27,11 @@ setup(
             include_dirs=[get_include()],
             sources=['gradunwarp/core/{}_ext.c'.format(mod)],
             extra_compile_args=['-O3'],
+            define_macros=[('Py_LIMITED_API', '0x03020000')],
+            py_limited_api=True,
         )
         for mod in ('interp3', 'legendre', 'transform_coordinates')
     ],
     scripts=['gradunwarp/core/gradient_unwarp.py'],
+    cmdclass={'bdist_wheel': bdist_wheel_abi3},
 )
